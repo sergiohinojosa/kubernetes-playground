43a0df151e59431f01f4fd7e465fd84e9bae9ddb
Sergio Hinojosa <sergio.hinojosa@dynatrace.com>
2022-12-15 20:37:50 +0100

    delete cementery
    Signed-off-by: Sergio Hinojosa <sergio.hinojosa@dynatrace.com>

--- apps/cementery/ingress/createingress.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-## Create Ingress Rules
-
-export PUBLIC_IP=$(curl -s ifconfig.me) 
-PUBLIC_IP_AS_DOM=$(echo $PUBLIC_IP | sed 's~\.~-~g')
-export DOMAIN="${PUBLIC_IP_AS_DOM}.nip.io"
-sed 's~domain.placeholder~'"$DOMAIN"'~' ingress.template > ingress.yaml
-kubectl apply -f ingress.yaml
--- apps/cementery/misc/.bash_aliases
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Alias for ease of use of the CLI
-alias las='ls -las' 
-alias hg='history | grep' 
-alias h='history' 
-alias vaml='vi -c "set syntax:yaml" -' 
-alias vson='vi -c "set syntax:json" -' 
-alias pg='ps -aux | grep'
