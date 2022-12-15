d225f68f0701114749292779a54409b71df62bf9
Sergio Hinojosa <sergio.hinojosa@dynatrace.com>
2022-12-15 19:56:58 +0100

    easytravel
    Signed-off-by: Sergio Hinojosa <sergio.hinojosa@dynatrace.com>

--- apps/easytravel-k8s/deploy-easytravel.sh
+++ apps/easytravel-k8s/deploy-easytravel.sh
@@ -6,14 +6,10 @@ source ../util/loaddomain.sh
 # Create namespace workshop
 kubectl create ns easytravel
 
-
-# Expose deployment of simplenode
-kubectl -n simplenode expose deployment simplenode --type=NodePort --port=8080 --name simplenode
-
 ## Create Ingress Rules
 sed 's~domain.placeholder~'"$DOMAIN"'~' manifests/ingress.template > manifests/ingress-gen.yaml
 
-kubectl -n simplenode apply -f manifests/ingress-gen.yaml
+kubectl -n easytravel apply -f manifests/
 
-echo "Simplnode is available here:"
-kubectl get ing -n simplenode
+echo "Easytravel is available here:"
+kubectl get ing -n easytravel
