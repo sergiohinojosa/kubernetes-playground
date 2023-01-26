cb524a3ed9b3ef0ec44ec42f3db46dc1f984d589
Sergio Hinojosa <sergio.hinojosa@dynatrace.com>
2023-01-26 12:39:13 +0100

    instrumenting nginx
    Signed-off-by: Sergio Hinojosa <sergio.hinojosa@dynatrace.com>

--- apps/nginx/instrument-nginx.sh
+++ apps/nginx/instrument-nginx.sh
@@ -11,10 +11,11 @@ else
     if kubectl get dynakubes.dynatrace.com -n dynatrace -o yaml | grep -q cloudNativeFullStack; then
         echo "CloudNative is active, setting the library for it."
         kubectl apply -f ingress-nginx-controller-microk8s-cn.yaml 
-        echo "Recycling the ingress so the CSI can be mounted."
-        kubectl delete pod -n ingress -l name=nginx-ingress-microk8s
     else   
         echo "ClassicFullStack is active, setting the library for it."
         kubectl apply -f ingress-nginx-controller-microk8s-classic.yaml 
     fi
+    
+    echo "Recycling the ingress pod so the library can be mounted."
+    kubectl delete pod -n ingress -l name=nginx-ingress-microk8s
 fi
