515e138f1c1769f515670f7a479d1b62c66d6860
danatrace <89346509+danatrace@users.noreply.github.com>
2023-07-17 19:37:45 +0200

    Create deploy-easytrade.sh

---
+++ apps/easytrade/deploy-easytrade.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+ 
+# Read the domain from CM
+source ../util/loaddomain.sh
+kubectl create namespace easytrade
+
+## Create Ingress Rules
+sed 's~domain.placeholder~'"$DOMAIN"'~' manifests/ingress.template > manifests/ingress-gen.yaml
+
+kubectl -n dbic apply -f manifests/
+
+echo "easytrade is available here:"
+kubectl get ing -n dbic
