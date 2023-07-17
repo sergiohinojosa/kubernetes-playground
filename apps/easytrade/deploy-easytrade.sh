8492021dd9baa470a07a715975f7e091150a5733
danatrace <89346509+danatrace@users.noreply.github.com>
2023-07-17 20:55:06 +0200

    Update deploy-easytrade.sh

--- apps/easytrade/deploy-easytrade.sh
+++ apps/easytrade/deploy-easytrade.sh
@@ -7,7 +7,7 @@ kubectl create namespace easytrade
 ## Create Ingress Rules
 sed 's~domain.placeholder~'"$DOMAIN"'~' manifests/ingress.template > manifests/ingress-gen.yaml
 
-kubectl -n dbic apply -f manifests/
+kubectl -n easytrade apply -f manifests/
 
 echo "easytrade is available here:"
 kubectl get ing -n easytrade
