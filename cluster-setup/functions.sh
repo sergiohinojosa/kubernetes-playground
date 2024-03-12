70101d375ece548a035640fbc053df2bf9ad8793
danatrace <89346509+danatrace@users.noreply.github.com>
2024-03-11 23:13:04 -0700

    Update functions.sh

--- cluster-setup/functions.sh
+++ cluster-setup/functions.sh
@@ -280,7 +280,7 @@ waitForAllPods() {
     namespace_filter="--all-namespaces"
   fi
   RETRY=0
-  RETRY_MAX=24
+  RETRY_MAX=60
   # Get all pods, count and invert the search for not running nor completed. Status is for deleting the last line of the output
   CMD="bashas \"kubectl get pods $namespace_filter 2>&1 | grep -c -v -E '(Running|Completed|Terminating|STATUS)'\""
   printInfo "Checking and wait for all pods in \"$namespace_filter\" to run."
